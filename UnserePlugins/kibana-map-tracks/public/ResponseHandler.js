7039d940ef0639ad07e252a8cbae33739ff1a422
Unknown <christin-boesch@web.de>
2019-02-13 13:28:40 +0100

    Visualisierung für das Test Dashboard

---
+++ UnserePlugins/kibana-map-tracks/public/ResponseHandler.js
@@ -0,0 +1,18 @@
+export function handleResponse(vis, response) {
+  return response.aggregations.sessions.buckets.map(session => {
+    const events = session.events.hits.hits.map(event => {
+      return {
+        center: {
+          x: event._source[vis.params.geoField].x,
+          y: event._source[vis.params.geoField].y
+        },
+        scale: event._source[vis.params.scaleField],
+        timestamp: event._source[vis.params.timeField]
+      };
+    });
+    return {
+      events,
+      session_key: session.key
+    };
+  });
+}
